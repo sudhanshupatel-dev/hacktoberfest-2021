8594506288bac7d5a48344e229f957b2e72cd47c
Shakthi Lahiru <42795355+shakthilahiru@users.noreply.github.com>
2021-10-01 10:35:39 +0530

    updated calander

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
 print("Hello World!!!")
+import calendar
+
+yy = 2021  # year
+mm = 10    # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
